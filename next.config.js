2f4c4750e29410eaac1fea33c19082b4ee3fa6db
28VYK <adrian.vichiriuc@student.usv.ro>
2026-05-31 22:20:52 +0300

    fix: remove deprecated interest-cohort from Permissions-Policy header

--- next.config.js
+++ next.config.js
@@ -51,7 +51,7 @@ const nextConfig = {
           },
           {
             key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+            value: 'camera=(), microphone=(), geolocation=()',
           },
         ],
       },
